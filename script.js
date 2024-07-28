0582aee3cb251379513b1ef83a30466fe56dc2b7
b.zamani@renolos.com <b.zamani@renolos.com>
2024-07-28 09:06:45 +0200

    add search function

--- script.js
+++ script.js
@@ -2,25 +2,32 @@
 let baseUrl = "https://pokeapi.co/api/v2/pokemon?limit=40&offset=0";
 let responseAsJson;
 let pokemonData = [];
+let previousInputLength = 0; 
 
 async function init() {
     await getApiPokemon();
     showPokCard();
+  
 }
 
 async function getApiPokemon() {
     let userResponse = await fetch(baseUrl);
     responseAsJson = await userResponse.json();
     pokemonData = responseAsJson.results;
-    console.log("result", responseAsJson.results);
 }
 
-async function showPokCard() {
+async function showPokCard(filteredData = []) {
     let content = document.getElementById('content');
+    let source = filteredData.length ? filteredData : pokemonData;
     content.innerHTML = '';
 
-    for (let i = 0; i < pokemonData.length; i++) {
-        let result = pokemonData[i];
+    if (filteredData.length === 0 && previousInputLength >= 3) {
+        content.innerHTML = '<p>No Pokémon found matching your search criteria.</p>';
+        return;
+    }
+
+    for (let i = 0; i < source.length; i++) {
+        let result = source[i];
         let response = await fetch(result.url);
         let pokemonDetails = await response.json();
         let pokemonImgUrl = pokemonDetails.sprites.other['official-artwork'].front_default;
@@ -51,14 +58,18 @@ async function showPokCard() {
 }
 
 
-
-
-
-
-        
-         
-
-
-
-
-
+function filterAndShowNames() {
+    let searchInput = document.getElementById('searchInput').value.toLowerCase();
+            
+    if (searchInput.length>=3) {     
+        let filteredData = pokemonData.filter(pokemon =>
+            pokemon.name.toLowerCase().startsWith(searchInput)
+        );
+        showPokCard(filteredData);
+    }
+    else if (previousInputLength >= 3 && searchInput.length < 3) {
+        previousInputLength = 0;
+        showPokCard();
+    }
+    previousInputLength = searchInput.length;
+}
